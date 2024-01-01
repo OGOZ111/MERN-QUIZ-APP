9b3782bedc05477a3aff99c06b0be5b1091cf03f
Luke B <142310217+OGOZ111@users.noreply.github.com>
2024-01-01 18:21:36 +0200

    created result component, added table which will populate scores and names from MongoDB backend

--- client/src/components/Questions.jsx
+++ client/src/components/Questions.jsx
@@ -16,7 +16,7 @@ export const Questions = () => {
 
   return (
     <div className="questions">
-      <h2 className="text-light">Simple Question 1</h2>
+      <h2 className="text-light">{question.question}</h2>
 
       <ul key={question.id}>
         {question.options.map((q, i) => (
@@ -32,7 +32,7 @@ export const Questions = () => {
             <label className="text-primary" htmlFor={`q${i}-option`}>
               {q}
             </label>
-            <div className="check checked"></div>
+            <div className="check "></div>
           </li>
         ))}
       </ul>
--- client/src/components/Result.jsx
+++ client/src/components/Result.jsx
@@ -1,5 +1,51 @@
 import React from "react";
+import "../styles/Result.css";
+import { Link } from "react-router-dom";
+import { ResultTable } from "./ResultTable";
 
 export const Result = () => {
-  return <div>Result</div>;
+  function onRestart() {
+    console.log("restarted");
+  }
+  return (
+    <div className="container">
+      <h1 className="title text-light">Quiz App</h1>
+
+      <div className="result flex-center">
+        <div className="flex">
+          <span>Username</span>
+          <span className="bold">Luke</span>
+        </div>
+        <div className="flex">
+          <span>Total Quiz Points: </span>
+          <span className="bold">50</span>
+        </div>
+        <div className="flex">
+          <span>Total Questions: </span>
+          <span className="bold">05</span>
+        </div>
+        <div className="flex">
+          <span>Total attempts: </span>
+          <span className="bold">03</span>
+        </div>
+        <div className="flex">
+          <span>Total Earned Points:</span>
+          <span className="bold">30</span>
+        </div>
+        <div className="flex">
+          <span>Quiz Result</span>
+          <span className="bold">Passed</span>
+        </div>
+      </div>
+      <div className="start">
+        <Link className="btn" to={"/"} onClick={onRestart}>
+          Restart
+        </Link>
+      </div>
+      <div className="container">
+        {/**Result table from back end */}
+        <ResultTable />
+      </div>
+    </div>
+  );
 };
