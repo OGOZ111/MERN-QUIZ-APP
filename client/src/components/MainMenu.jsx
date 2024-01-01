36e305d2f7ff0a9d77e22dc3e7a2269cfd4add3e
Luke B <142310217+OGOZ111@users.noreply.github.com>
2024-01-01 16:37:52 +0200

    created mainmenu component, added user rules, input for name, start btn which links to quiz component. username saved in UseRef

--- client/src/components/MainMenu.jsx
+++ client/src/components/MainMenu.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
 
 export const MainMenu = () => {
-  return <div>Page</div>;
+  const inputRef = useRef(null);
+
+  return (
+    <div className="container">
+      <h1 className="title text-light">Quiz App</h1>
+
+      <ol>
+        <li>You will be asked 10 questions one after another.</li>
+        <li>10 points is awarded for the correct answer.</li>
+        <li>
+          Each question has three options. You can choose only one options.
+        </li>
+        <li>You can review and change answers before the quiz finish.</li>
+        <li>The result will be declared at the end of the quiz.</li>
+      </ol>
+      <form id="form">
+        <input ref={inputRef} type="text" placeholder="Enter username" />
+      </form>
+
+      <div className="start">
+        <Link className="btn" to={"quiz"}>
+          Start Quiz
+        </Link>
+      </div>
+    </div>
+  );
 };
