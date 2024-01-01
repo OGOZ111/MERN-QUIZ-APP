426abb5adb925cc7ceff29e851cfb31b55d0fba8
Luke B <142310217+OGOZ111@users.noreply.github.com>
2024-01-01 17:58:43 +0200

    mapped key and value questions from local json file to question app, in radio input

--- client/src/components/Questions.jsx
+++ client/src/components/Questions.jsx
@@ -4,8 +4,10 @@ import data from "../database/data";
 export const Questions = () => {
   const [checked, setChecked] = useState(undefined);
 
+  const question = data[0];
+
   useEffect(() => {
-    console.log(data);
+    console.log(question);
   });
 
   function onSelect() {
@@ -16,21 +18,23 @@ export const Questions = () => {
     <div className="questions">
       <h2 className="text-light">Simple Question 1</h2>
 
-      <ul>
-        <li>
-          <input
-            type="radio"
-            value={false}
-            name="options"
-            id="q1-option"
-            onChange={onSelect()}
-          />
+      <ul key={question.id}>
+        {question.options.map((q, i) => (
+          <li key={i}>
+            <input
+              type="radio"
+              value={false}
+              name="options"
+              id={`q${i}-option`}
+              onChange={onSelect()}
+            />
 
-          <label className="text-primary" htmlFor="q1-option">
-            Option
-          </label>
-          <div className="check checked"></div>
-        </li>
+            <label className="text-primary" htmlFor={`q${i}-option`}>
+              {q}
+            </label>
+            <div className="check checked"></div>
+          </li>
+        ))}
       </ul>
     </div>
   );
