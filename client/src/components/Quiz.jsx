90d07d3745811c2db5c267412e47942ecc4306cb
Luke B <142310217+OGOZ111@users.noreply.github.com>
2024-01-02 15:39:39 +0200

    added if statement (result.length && result.length >= queue.length) it will automitcally end  the quiz, return the user to the results page, wit the saved array of results, now saved in a varaible

--- client/src/components/Quiz.jsx
+++ client/src/components/Quiz.jsx
@@ -6,15 +6,16 @@ import { PushAnswer } from "../hooks/setResult";
 //redux store import
 
 import { useSelector, useDispatch } from "react-redux";
+import { Navigate } from "react-router-dom";
 
 export default function Quiz() {
   const [check, setChecked] = useState(undefined);
-  const state = useSelector((state) => state);
+  const result = useSelector((state) => state.result.result);
   const { queue, trace } = useSelector((state) => state.questions);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    console.log(state);
+    //console.log(state);
   });
 
   //Button event handler for next and prev buttons
@@ -23,7 +24,7 @@ export default function Quiz() {
     if (trace < queue.length) {
       // Update the trace value by +1, which moves to the next index in the array of questions using move next action
       dispatch(MoveNextQuestion());
-      dispatch(PushAnswer(1));
+      dispatch(PushAnswer(check));
     }
   }
 
@@ -39,6 +40,10 @@ export default function Quiz() {
     setChecked(check);
   }
 
+  // finished exam after last question
+  if (result.length && result.length >= queue.length) {
+    return <Navigate to={"/result"} replace="true"></Navigate>;
+  }
   return (
     <div className="container">
       <h1 className="title text-light">Quiz App</h1>
