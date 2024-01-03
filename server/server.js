1eb792003c4f25d18b6de55f5010761c0e48a059
Luke B <142310217+OGOZ111@users.noreply.github.com>
2024-01-03 17:05:12 +0200

    created development server, added nodemon start to scripts, port 8080

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
-const express = require("express");
+import express from "express";
+
+const app = express();
+
+// routes
+
+app.get("/", (req, res) => {
+  try {
+    res.json("Get Request");
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.listen(8080, () => {
+  console.log("Server connected to http://localhost:8080");
+});
